29aa6a33343e4b9fe7f40475bcc0bdbe95181b87
Dhanajit Kapali <37000235+dhanajitkapali@users.noreply.github.com>
2022-08-07 17:19:44 +0530

    added some animations

--- Dog Facts/New Facts/Views/NewFactsViewController.swift
+++ Dog Facts/New Facts/Views/NewFactsViewController.swift
@@ -12,16 +12,42 @@ class NewFactsViewController: UIViewController {
     // MARK: - Private Data Members
     private var presenter : NewFactsPresenter!
     
+    private let dogImageContainer : UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let dogImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.image = UIImage(named: "dog-58")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+
+}
+
+
+// MARK: - LifeCycle Methods
+extension NewFactsViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initialSetup()
         
         addGradientLayer()
         
+        self.view.addSubview(dogImageView)
+        animateDogImage()
+        
         presenter.getNewDogFact()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //dogImageView.frame = CGRect(x: self.view.center.x-50, y: self.view.center.y, width: 100, height: 100)
+        //dogImageView.center = self.view.center
+    }
 }
 
 
@@ -43,6 +69,17 @@ extension NewFactsViewController {
         ]
         gradientLayer.colors = gradientColors
         self.view.layer.addSublayer(gradientLayer)
+    }
+    
+    func animateDogImage() {
+        dogImageView.center = self.view.center
+        UIView.animate(withDuration: 2, animations: {
+            //self.dogImageView.center = newCenter
+            self.dogImageView.frame = CGRect(x: self.view.center.x-50, y: self.view.center.y-300, width: 100, height: 100)
+        }, completion: { done in
+            self.presenter.getNewDogFact()
+        })
+
     }
 }
 
